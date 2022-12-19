4ac5a616ae1461b16783ded93c019b5aa7cbaeb1
Jelizaveta Ivanova <jelizaveta.ivanova23@gmail.com>
2022-12-19 23:59:32 +0200

    first commit

---
+++ src/Class.js
@@ -0,0 +1,11 @@
+// ne objazateljno, dlja primera, eto starqj variant
+
+import React from 'react';
+
+export default class ClassComponent extends React.Component {
+    render() {
+        return (
+            <div>Privet ot ClassComponent</div>
+        )
+    }
+}
